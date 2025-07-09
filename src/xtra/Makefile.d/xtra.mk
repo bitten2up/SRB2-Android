5a571ab1e066d6eabcdf1734028d45d214a88c09
bitten2up <seant9140@gmail.com>
2025-07-09 09:37:22 -0500

    bitten test IGNORE

--- src/xtra/Makefile.d/xtra.mk
+++ src/xtra/Makefile.d/xtra.mk
@@ -3,39 +3,59 @@
 # XTRA Edition!
 #
 
-CURRENT_OPTS := 
-
-undefine NOHW
-HWRENDER=1
-
-sources+=w_handle.c
-sources+=\
-	$(call List,android/Sourcefile)\
-	$(call List,xtra/Sourcefile)\
-
-opts+=-DTOUCHINPUTS -DNATIVESCREENRES # -DHAVE_WHANDLE
-
-#opts+=-DHWRENDER
-opts+=-DHWRENDER -DHAVE_GLES -DHAVE_GLES2
+CURRENT_OPTS:= 
+CURRENT_SOURCES:=
 
 ifndef ANDROID
-  CURRENT_OPTS=opts
+  opts+=-DTOUCHINPUTS -DNATIVESCREENRES # -DHAVE_WHANDLE
+  ifneq ($(findstring -DHWRENDER, $(opts)),)
+    opts+=-DHAVE_GLES -DHAVE_GLES2
+  endif
+  CURRENT_OPTS+=$(opts)
+  #CURRENT_SOURCES:=$(sources)
 else
-  CURRENT_OPTS=LOCAL_CFLAGS
+  CURRENT_OPTS+=$(LOCAL_CFLAGS)
+  #CURRENT_SOURCES:=$(LOCAL_SRC_FILES)
+endif
+
+ifndef ANDROID
+LOCAL_PATH:=.
+SRC_MAIN:=$(LOCAL_PATH)
+SRC_HWR:=hardware
+SRC_SDL:=sdl
+SRC_APK:=android
+SRC_XTRA:=xtra
+endif
+
+CURRENT_SOURCES+=$(SRC_MAIN)/w_handle.c
+ifndef ANDROID
+
+CURRENT_SOURCES+=\
+  $(call List,$(LOCAL_PATH)/$(SRC_APK)/Sourcefile)\
+  $(call List,$(LOCAL_PATH)/$(SRC_XTRA)/Sourcefile)\
+
 endif
 
 ifndef NOHW
-  $(info CHECKING FOR PROPER OPENGL...)
-  ifeq (, $(findstring -DHWRENDER, $(CURRENT_OPTS)))
-    ifeq (, $(findstring -DHAVE_GLES2, $(CURRENT_OPTS)))
-      $(info BRO PLEASE)
-      sources+=hardware/r_gles/r_gles2.c sdl/ogl_es_sdl.c
-    else ifeq (, $(findstring -DHAVE_GLES, $(CURRENT_OPTS)))
-      $(info BRO PLEASE GLES1)
-      sources+=hardware/r_gles/r_gles1.c sdl/ogl_es_sdl.c
-    else
-      $(info BRO PLEASE GLES2)
-      sources+=hardware/r_opengl/r_opengl.c sdl/ogl_sdl.c
-    endif
+  #ifeq (, $(findstring -DHWRENDER, $(CURRENT_OPTS)))
+  #ifdef HAVE_GLES2
+  ifneq ($(findstring -DHAVE_GLES2, $(CURRENT_OPTS)),)
+    CURRENT_OPTS+=-DHAVE_GLES2
+    CURRENT_SOURCES+=$(SRC_HWR)/r_gles/r_gles2.c $(SRC_SDL)/ogl_es_sdl.c
+  #else ifdef HAVE_GLES
+  #else ifeq (, $(findstring -DHAVE_GLES,$(CURRENT_OPTS)))
+  else ifneq ($(findstring -DHAVE_GLES, $(CURRENT_OPTS)),)
+    CURRENT_OPTS+=-DHAVE_GLES
+    CURRENT_SOURCES+=$(SRC_HWR)/r_gles/r_gles1.c $(SRC_SDL)/ogl_es_sdl.c
+  else
+    CURRENT_SOURCES+=$(SRC_HWR)/r_opengl/r_opengl.c $(SRC_SDL)/ogl_sdl.c
   endif
+endif
+
+ifndef ANDROID
+  opts+=$(CURRENT_OPTS)
+  sources+=$(CURRENT_SOURCES)
+else
+  LOCAL_CFLAGS+=$(CURRENT_OPTS)
+  LOCAL_SRC_FILES+=$(CURRENT_SOURCES)
 endif
